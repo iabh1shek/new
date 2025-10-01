04fa8616f081742019173109d64a89f6a5cfd649
iabh1shek <abhishek.aug122001@gmail.com>
2025-10-01 18:44:50 +0530

    java practice

---
+++ hello.java
@@ -0,0 +1,10 @@
+public class Main{ 
+
+    public static void main(String[] args)  { 
+        // data types 
+        int value = 20; 
+        String string = "abhishek" 
+        float value = 20.9; 
+
+    }
+}
